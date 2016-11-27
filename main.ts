a4833148e3fff14721afe2cf7a7073d64ec48d70
Richard Seldon <arcseldon@gmail.com>
2016-11-27 21:38:24 +0900

    mouse event

--- main.ts
+++ main.ts
@@ -1,27 +1,12 @@
-// import { Observable, Observer } from "rxjs";
+import { Observable, Observer } from "rxjs";
 
-import {Observable} from "rxjs/Observable";
-import {Observer} from "rxjs/Observer";
+let source = Observable.fromEvent(document, "mousemove");
 
-import "rxjs/add/operator/map"
-import "rxjs/add/operator/filter"
-
-let numbers = [1, 5, 10];
-let source = Observable.create(observer => {
-
-  // for (let n of numbers) {
-  //   debugger;
-  //   observer.next(n);
-  // }
-
-  numbers.forEach((n) => observer.next(n));
-  observer.complete();
-
-}).map(x => x * 2)
-.filter(x => x > 5);
+// .map(x => x * 2)
+// .filter(x => x > 5);
 
 source.subscribe(
-  value => console.log(`value : ${value}`),
+  value => console.log(value),
   (err) => console.log(`error : ${err}`),
   () => console.log('complete')
 );
