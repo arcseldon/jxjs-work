2e57920209da4ec7758ee3ef2e84943f6c67225c
Richard Seldon <arcseldon@gmail.com>
2016-11-27 21:21:36 +0900

    add map and filter

--- main.ts
+++ main.ts
@@ -3,6 +3,9 @@
 import {Observable} from "rxjs/Observable";
 import {Observer} from "rxjs/Observer";
 
+import "rxjs/add/operator/map"
+import "rxjs/add/operator/filter"
+
 let numbers = [1, 5, 10];
 let source = Observable.create(observer => {
 
@@ -14,7 +17,8 @@ let source = Observable.create(observer => {
   numbers.forEach((n) => observer.next(n));
   observer.complete();
 
-});
+}).map(x => x * 2)
+.filter(x => x > 5);
 
 source.subscribe(
   value => console.log(`value : ${value}`),
