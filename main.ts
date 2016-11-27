acabe952eee6deb366fa070d5edcc6d8a09020b1
Richard Seldon <arcseldon@gmail.com>
2016-11-27 20:21:29 +0900

    first rxjs working example

--- main.ts
+++ main.ts
@@ -1,1 +1,22 @@
-alert("ok ");
+import {Observable} from "rxjs";
+
+let numbers = [1,5,10];
+let source = Observable.from(numbers);
+
+class MyObserver {
+
+  next(value) {
+    console.log(`value : ${value}`);
+  }
+
+  error(err) {
+    console.log(`error : ${err}`);
+  }
+
+  complete() {
+    console.log('complete');
+  }
+
+}
+
+source.subscribe(new MyObserver());
